102203a5bf2fbeb91ab4d93cd5bdef3afb3e3e8c
yaroslav <maheyyaroslav2006@gmail.com>
2025-07-27 16:05:46 +0300

    merge actual main

--- src/App.tsx
+++ src/App.tsx
@@ -9,49 +9,49 @@ import Billing from "@/pages/Settings/Billing";
 import Limits from "@/pages/Settings/Limits";
 
 
-import { ToastContainer } from "react-toastify";
-import { ROUTS } from "@/routes/routes.tsx";
-import { ProtectedRoute } from "@/auth/ProtectedRoute.tsx";
+import {ToastContainer} from "react-toastify";
+import {ROUTS} from "@/routes/routes.tsx";
+import {ProtectedRoute} from "@/auth/ProtectedRoute.tsx";
 import {Route, Routes} from "react-router";
-import { Layout } from "@/components/Layout";
+import {Layout} from "@/components/Layout";
 
 export function App() {
     return (
         <>
             <Routes>
-                <Route path={ROUTS.LOGIN} element={<LoginPage />} />
-                <Route path={ROUTS.REGISTER} element={<RegisterPage />} />
-                <Route element={<ProtectedRoute />}>
-                    <Route path={ROUTS.HOME} element={<Home />} />
-                    <Route path={ROUTS.DASHBOARD} element={<div>Dashboard Page</div>} />
-                    <Route path={ROUTS.PROFILE} element={<div>Profile Page</div>} />
+                <Route path={ROUTS.LOGIN} element={<LoginPage/>}/>
+                <Route path={ROUTS.REGISTER} element={<RegisterPage/>}/>
 
-<Route path={ROUTS.SETTINGS.ROOT} element={<div>Settings Page</div>} />
-<Route path={ROUTS.SETTINGS.GENERAL} element={<General />} />
-<Route path={ROUTS.SETTINGS.TEAM} element={<Team />} />
-<Route path={ROUTS.SETTINGS.BILLING} element={<Billing />} />
-<Route path={ROUTS.SETTINGS.LIMITS} element={<Limits />} />
+                <Route element={<Layout/>}>
+                <Route element={<ProtectedRoute/>}>
+                    <Route path={ROUTS.HOME} element={<Home/>}/>
+                    <Route path={ROUTS.DASHBOARD} element={<div>Dashboard Page</div>}/>
+                    <Route path={ROUTS.PROFILE} element={<div>Profile Page</div>}/>
 
-
-                    <Route path={ROUTS.ACCOUNT} element={<Account />} />
-                    <Route path={ROUTS.SAVED} element={<div>Saved Page</div>} />
-                    <Route path={ROUTS.EDIT} element={<div>Edit Page</div>} />
+                    <Route path={ROUTS.SETTINGS.ROOT} element={<div>Settings Page</div>}/>
+                    <Route path={ROUTS.SETTINGS.GENERAL} element={<General/>}/>
+                    <Route path={ROUTS.SETTINGS.TEAM} element={<Team/>}/>
+                    <Route path={ROUTS.SETTINGS.BILLING} element={<Billing/>}/>
+                    <Route path={ROUTS.SETTINGS.LIMITS} element={<Limits/>}/>
 
 
+                    <Route path={ROUTS.ACCOUNT} element={<Account/>}/>
+                    <Route path={ROUTS.SAVED} element={<div>Saved Page</div>}/>
+                    <Route path={ROUTS.EDIT} element={<div>Edit Page</div>}/>
 
-                    <Route element={<Layout />}>
-                        <Route path={ROUTS.HOME} element={<Home />} />
-                        <Route path={ROUTS.DASHBOARD} element={<div>Dashboard Page</div>} />
-                        <Route path={ROUTS.PROFILE} element={<div>Profile Page</div>} />
-                        <Route path={ROUTS.SETTINGS} element={<div>Settings Page</div>} />
-                        <Route path={ROUTS.ACCOUNT} element={<Account />} />
-                        <Route path={ROUTS.SAVED} element={<div>Saved Page</div>} />
-                        <Route path={ROUTS.EDIT} element={<div>Edit Page</div>} />
+
+                        <Route path={ROUTS.HOME} element={<Home/>}/>
+                        <Route path={ROUTS.DASHBOARD} element={<div>Dashboard Page</div>}/>
+                        <Route path={ROUTS.PROFILE} element={<div>Profile Page</div>}/>
+                        {/*<Route path={ROUTS.SETTINGS} element={<div>Settings Page</div>}/>*/}
+                        <Route path={ROUTS.ACCOUNT} element={<Account/>}/>
+                        <Route path={ROUTS.SAVED} element={<div>Saved Page</div>}/>
+                        <Route path={ROUTS.EDIT} element={<div>Edit Page</div>}/>
                     </Route>
                 </Route>
             </Routes>
 
-            <ToastContainer autoClose={1000} />
+            <ToastContainer autoClose={1000}/>
         </>
     );
 }
